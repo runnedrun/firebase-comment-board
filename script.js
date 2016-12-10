fb75f793fdf880678a46e8ab23ebe22e5cc8b52a
David Gaynor <runnedrun@gmail.com>
2016-12-10 14:47:46 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var commentInput = $("#comment-input")
-var commentDisplay = $(".not-actually-the-right-class")
+var editor = $(parent.document.body)
+var topbar = $(parent.parent.document.body).find("#topbar")
 
-commentInput.keydown(function(e) {
-  if (e.which == 13) {
-    addListItem("comments", commentInput.val());
-  }
-})
+var allDivsInTheEditor = editor.find("div")
 
-onNewListItem("whats-my-name-again", function(newComment) {
-  commentDisplay.append("<span>" + newComment + "</span>")
-})
+
+
+// Now try to
+// 1. add a button to the topbar
+// 2. change the title above the file list
+// 3. change some borders in the editor
+// 4. Make all this happen when a button is clicked
